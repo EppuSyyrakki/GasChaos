376fe039ca6cc70de9db90895f9ceb4ec31b7787
Aleksi Pynnonen <iskela45@gmail.com>
2020-04-17 17:57:10 +0300

    Added HighScore.java Added HighScoreEntry.java Added HighScoreListener.java Added HighScoreServer.java Added highscore.config Added Request for internet access to AndroidManifest.xml Added <action android:name="android.intent.action.VIEW" /> to AndroidManifest.xml

---
+++ core/src/fi/tuni/tiko/HighScoreEntry.java
@@ -0,0 +1,33 @@
+package fi.tuni.tiko;
+
+
+public class HighScoreEntry {
+    private String name;
+    private int score;
+
+    // Needed for json parsing, same reason as with Cow.java and Field.java.
+    public HighScoreEntry() {
+    }
+
+    public HighScoreEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+    // Getters and setters ...
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
